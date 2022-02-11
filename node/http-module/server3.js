fce46de31b232f84050662d0246e79006925e183
meanstack <meanstack4@gmail.com>
2022-02-11 11:51:32 +0530

    node app using mysql

--- node/http-module/server3.js
+++ node/http-module/server3.js
@@ -5,22 +5,23 @@ http.createServer((request,response)=>{
    //console.log(requesteUrl);
     //response.writeHead(200,{'Content-Type':'html/text'});
    if(requestedUrl.pathname == "/" || requestedUrl.pathname =="/home"){
-       //response.writeHead(200,{'Content-Type':'text/html'});
+       response.writeHead(200,{'Content-Type':'text/html'});
        response.write("Hello Node.."); 
        response.write("<h1>Home Page</h1>"); 
        response.end(); 
    }
    else if(requestedUrl.pathname == '/about'){
-    //response.writeHead(200,{'Content-Type':'html/text'});
+    response.writeHead(200,{'Content-Type':'html/text'});
     response.write("<h1>About Page</h1>"); 
     response.end();
    }
    else if(requestedUrl.pathname == "/contact"){
-    //response.writeHead(200,{'Content-Type':'html/text'});
+    response.writeHead(200,{'Content-Type':'text/html'});
     response.write("<h1>Contact Page</h1>"); 
     response.end();
    }
    else if(requestedUrl.pathname == '/add'){
+      response.writeHead(200,{'Content-Type':'text/html'}); 
       let a = requestedUrl.query.a * 1;
       let b = requestedUrl.query.b*1;
       response.end("<h1>Addition : "+(a+b)+"</h1>");
